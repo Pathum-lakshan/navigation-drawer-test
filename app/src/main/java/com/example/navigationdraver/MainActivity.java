c0d9fd16b67adf1fe3c9998c44cf2e52e047c758
pathumlakshan1101 <lakshanherath1101@gmail.com>
2022-07-18 09:26:21 +0530

    remove some comment for test pushing

--- app/src/main/java/com/example/navigationdraver/MainActivity.java
+++ app/src/main/java/com/example/navigationdraver/MainActivity.java
@@ -20,8 +20,7 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.my_drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
 
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
+
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         ActionBar actionBar = getSupportActionBar();
@@ -29,11 +28,6 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    // override the onOptionsItemSelected()
-    // function to implement
-    // the item click listener callback
-    // to open and close the navigation
-    // drawer when the icon is clicked
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
